59af3226978d539830ee40268e5c51ac874c4d1d
Mario "Kuroir" Ricalde <kuroir@gmail.com>
2013-03-08 13:59:47 -0600

    Adds the Posts Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 KidsCodeCamp::Application.routes.draw do
-  root :to => 'home#index'
+  root :to => 'posts#index'
+  resources :posts
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
