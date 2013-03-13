bbb20248bdf6a54f112635fbeb6c3cdef5fed9e6
Daniel Portales <dportalesr@gmail.com>
2013-03-13 14:50:23 -0600

    public and admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 KidsCodeCamp::Application.routes.draw do
   root :to => 'posts#index'
-  resources :posts, :events
-  namespace :admin do
-    match '/' => 'users#index'
-    resources :users
-  end
+  
+  # Devise
   devise_for :users, :path_names => { :sign_in => "login", :sign_out => "logout" }, :path => "d"
-  resources :users
+  devise_scope :user do
+    match "admin" => "devise/sessions#new"
+  end
+
+  # Admin
+  namespace :admin , :only => [:new, :create, :edit, :update, :delete, :index ] do
+    resources :users, :posts, :events
+  end
+  
+  # Public
+  scope :only => [:index, :show] do
+    resources :users, :posts, :events
+  end
 end
