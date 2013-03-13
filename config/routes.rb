b9b538c6394f4bafeb02db3f808f7029c1e33246
Daniel Portales <dportalesr@gmail.com>
2013-03-13 07:57:58 -0600

    Devise and Admin namespace files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 KidsCodeCamp::Application.routes.draw do
-  devise_for :users
-
   root :to => 'posts#index'
-  resources :posts
+  resources :posts, :events
+  namespace :admin do
+    match '/' => 'users#index'
+    resources :users
+  end
+  devise_for :users, :path_names => { :sign_in => "login", :sign_out => "logout" }, :path => "d"
+  resources :users
 end
