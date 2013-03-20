a0d13629ef89806b29436c7d3dd55bc9d578c7a2
Daniel Portales <dportalesr@gmail.com>
2013-03-19 18:13:35 -0600

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,28 @@
 KidsCodeCamp::Application.routes.draw do
   root :to => 'events#index'
-  match 'signup' => 'pages#signup'
+  
+  match 'sign_up' => 'users/registrations#new'
+  match 'sing_in' => 'devise/sessions#new'
   match 'about' => 'pages#about'
 
   match 'contact' => 'contact_forms#new', :via => :get
   match 'contact' => 'contact_forms#create', :via => :post
-  match "admin" => "admin/users#dashboard"
+  match 'admin' => 'admin/users#dashboard'
 
   # Devise
-  devise_for :users, :controllers => { :registrations => "users/registrations" }
+  devise_for :users, :controllers => { :registrations => 'users/registrations' }
 
   # Admin
   namespace :admin , :only => [:new, :create, :edit, :update, :destroy, :index ] do
     resources :posts, :events
     resources :users do
-      get "dashboard", :on => :collection
+      get 'dashboard', :on => :collection
     end
   end
   
   # Public
   scope :only => [:index, :show] do
     resources :posts, :path => 'community'
-    resources :users, :posts, :events
+    resources :users, :events
   end
 end
