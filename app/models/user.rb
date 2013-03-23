910e30f329afa977408de5019ef36a713b70064e
Daniel Portales <dportalesr@gmail.com>
2013-03-22 18:29:45 -0600

    Updated User validation rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
   has_many :posts
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+  # devise modules: :token_authenticatable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
+  validates :username, :presence => true
+  validates :username, :uniqueness => { :case_sensitive => false }, :on => :create
+  validates :password_confirmation, :presence => true, :on => :create
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :is_admin
-  # attr_accessible :title, :body
 end
