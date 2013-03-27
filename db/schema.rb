d31cc871304e30a4ae6096cb42e2d2421f7b070d
Daniel Portales <dportalesr@gmail.com>
2013-03-27 16:19:20 -0600

    Rails_admin costumization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130322083953) do
+ActiveRecord::Schema.define(:version => 20130327200101) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -25,12 +25,25 @@ ActiveRecord::Schema.define(:version => 20130322083953) do
     t.string   "title"
     t.string   "slug"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
     t.integer  "user_id"
     t.integer  "parent_id"
-    t.string   "category"
+    t.string   "category",   :default => "General"
   end
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
