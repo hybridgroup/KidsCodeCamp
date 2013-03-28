346944213d3f35207b2e7d86f6df281e5bd561d0
Daniel Portales <dportalesr@gmail.com>
2013-03-28 11:58:05 -0600

    post controller CRUD and routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
-  # GET /posts
-  # GET /posts.json
+  #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   def index
     conditions = {:parent_id => nil}
 
@@ -22,9 +21,7 @@ class PostsController < ApplicationController
       format.json { render json: @posts }
     end
   end
-
-  # GET /posts/1
-  # GET /posts/1.json
+  #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   def show
     @current_post = Post.find(params[:id])
     @posts = Post.where(:parent_id => params[:id]).paginate(:page => params[:page], :per_page => 8).order("created_at DESC")
@@ -35,4 +32,56 @@ class PostsController < ApplicationController
       format.json { render json: @post }
     end
   end
+  #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  def new
+    @post = Post.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @post }
+    end
+  end
+  #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  def edit
+    @post = Post.find(params[:id])
+  end
+  #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  def create
+    params[:post][:user_id] = current_user.id if params[:post][:user_id].nil?
+    @post = Post.new(params[:post])
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to :after_save_redirect, notice: 'Post was successfully created.' }
+        format.json { render json: @post, status: :created, location: @post }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  def update
+    @post = Post.find(params[:id])
+
+    respond_to do |format|
+      if @post.update_attributes(params[:post])
+        format.html { redirect_to :after_save_redirect, notice: 'Post was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    respond_to do |format|
+      format.html { redirect_to :after_delete_redirect, notice: 'Post was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end 
 end
