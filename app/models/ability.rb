25d8e07cd979fc44dfd4230e0e056e660dd26a53
Daniel Portales <dportalesr@gmail.com>
2013-03-28 23:19:14 -0600

    New rails_admin + cancan config; not working

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, :all
     if user
-      can :access, :rails_admin
-      can :dashboard              # grant access to the dashboard
-      if user.is_admin == 1
-        can :manage, :all
-      else
-        can [:new, :create, :index], Post
-        can [:edit, :update, :destroy], Post, :user_id => user.id
-      end
+      can [:new, :create], Post
+      can [:edit, :update, :destroy], Post, :user_id => user.id
     end
   end
 end
