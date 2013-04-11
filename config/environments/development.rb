fcb7fef5db17c60b219189a74e1805dc7dda809c
Daniel Portales <dportalesr@gmail.com>
2013-04-11 17:00:02 -0500

    Sendgrid config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ KidsCodeCamp::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
@@ -45,6 +46,5 @@ KidsCodeCamp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.middleware.use ::Rack::FirebugLogger
 end
