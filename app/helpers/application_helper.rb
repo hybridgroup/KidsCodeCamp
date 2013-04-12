21c47352d15ca500aa65a028b6593cbffd03efaf
Daniel Portales <dportalesr@gmail.com>
2013-04-12 13:43:27 -0500

    Support for embedded youtube videos w markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     image_tag("http://gravatar.com/avatar/#{gravatar_id}.png?s=#{opts[:size]}", opts[:html])
   end
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::XHTML.new(:filter_html => true, :safe_links_only => true), :no_intra_emphasis => true)
+    raw markdown.render(text).gsub(/\[youtube\s+(.*?)\]/, "<iframe class='youtube-player' type='text/html' width='550' height='330' src='http://www.youtube.com/embed/$1' allowfullscreen frameborder='0'></iframe>")
+  end
 end
