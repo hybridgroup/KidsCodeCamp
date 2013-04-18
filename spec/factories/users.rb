50a999851ce5fd6088962c449efa5ef04b79d454
Daniel Portales <dportalesr@gmail.com>
2013-04-17 23:33:10 -0500

    Reqs user regs and sessions

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,8 @@ require 'faker'
 FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
-    password 'secret'
+    password 'ultrasecret'
+    confirm 'ultrasecret'
 
     factory :admin do
       admin true
--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe "user registration" do
+describe "user registrations" do
   context "with valid data" do
     it "allows new users to register with an email address, password and username" do
       visit sign_up_path
@@ -11,11 +11,13 @@ describe "user registration" do
       fill_in "Password confirmation", :with => "never_gone_give_you_up"
 
       click_button "Sign up"
-
-      page.should have_content("Welcome! You have signed up successfully.")
+      within ".flash_message" do
+        page.should have_content("Welcome! You have signed up successfully.")
+      end
     end
   end
   
+
   context "with invalid data" do
     it "disallow new users to register" do
       visit sign_up_path
@@ -27,7 +29,26 @@ describe "user registration" do
 
       click_button "Sign up"
 
-      page.should have_content("prohibited this user from being saved")
+      within "#error_explanation" do
+        page.should have_content("prohibited this user from being saved")
+      end
+    end
+  end
+end
+
+describe "user sessions" do
+  it "allows users to sign in after they have registered" do
+    user = FactoryGirl.create(:user)
+
+    visit login_path
+
+    fill_in "Email",    :with => user.email
+    fill_in "Password", :with => user.password
+
+    click_button "Sign in"
+
+    within ".flash_message" do
+      page.should have_content("Signed in successfully")
     end
   end
 end
