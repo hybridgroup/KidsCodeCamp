d76b4dbe3137267df109edb10fb69d2ceeca91f4
Daniel Portales <dportalesr@gmail.com>
2013-04-18 00:14:25 -0500

    Reqs valid/invalid credentials

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,19 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do
+    username { Faker::Internet.user_name }
     email { Faker::Internet.email }
     password 'ultrasecret'
-    confirm 'ultrasecret'
+    password_confirmation 'ultrasecret'
+    is_admin true
+  end
 
-    factory :admin do
-      admin true
-    end
+  factory :editor_user, parent: :user do
+    is_admin false
+  end
+
+  factory :invalid_user, parent: :user do
+    username { Faker::Internet.user_name }
+    email "invalid"
   end
 end
--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -11,9 +11,7 @@ describe "user registrations" do
       fill_in "Password confirmation", :with => "never_gone_give_you_up"
 
       click_button "Sign up"
-      within ".flash_message" do
-        page.should have_content("Welcome! You have signed up successfully.")
-      end
+      page.find('.flash_message').should have_content("Welcome! You have signed up successfully.")
     end
   end
   
@@ -29,26 +27,57 @@ describe "user registrations" do
 
       click_button "Sign up"
 
-      within "#error_explanation" do
-        page.should have_content("prohibited this user from being saved")
-      end
+      page.find('#error_explanation').should have_content("prohibited this user from being saved")
     end
   end
 end
 
 describe "user sessions" do
-  it "allows users to sign in after they have registered" do
-    user = FactoryGirl.create(:user)
+  describe "with valid credentials" do
+    describe "for admin users" do
+      it "allows users to sign in after they have registered" do
+        user = FactoryGirl.create(:user)
 
-    visit login_path
+        visit login_path
 
-    fill_in "Email",    :with => user.email
-    fill_in "Password", :with => user.password
+        fill_in "Email",    :with => user.email
+        fill_in "Password", :with => user.password
 
-    click_button "Sign in"
+        click_button "Sign in"
 
-    within ".flash_message" do
-      page.should have_content("Signed in successfully")
+        page.find('h1').should have_content("Site administration")
+      end
+    end
+
+
+    describe "for editor users" do
+      it "allows users to sign in after they have registered" do
+        user = FactoryGirl.create(:editor_user)
+
+        visit login_path
+
+        fill_in "Email",    :with => user.email
+        fill_in "Password", :with => user.password
+
+        click_button "Sign in"
+
+        page.find('.flash_message').should have_content("Signed in successfully")
+      end
+    end
+  end
+  
+  describe "with invalid credentials" do
+    it "denies login" do
+      user = FactoryGirl.create(:user)
+
+      visit login_path
+
+      fill_in "Email",    :with => ''
+      fill_in "Password", :with => user.password
+
+      click_button "Sign in"
+
+      page.find('.flash_message').should have_content("Invalid email or password")
     end
   end
 end
